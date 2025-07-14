d7daf64dbc5b5a9ca14d4db8b43e8203f9b03117
Nitish Singh <nitishsingh925@gmail.com>
2025-07-14 15:57:45 +0530

    Number Pyramid

---
+++ star_pattern/3_numberPyramid.js
@@ -0,0 +1,23 @@
+/*
+ * Number Pyramid Pattern Right Angle Triangle Matrix
+ * This code generates a right angle triangle matrix of numbers based on the input size.
+ * It also counts the total number of stars printed.
+ */
+
+console.log("Number Pyramid Pattern Right Angle Triangle Matrix");
+
+function starPattern(n) {
+  console.log("Number Pattern for Given Number  =", n);
+  let totalStar = 0;
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+      row = row + (j + 1);
+      totalStar++;
+    }
+    console.log(row);
+  }
+  console.log("Total Star ==>", totalStar);
+}
+
+starPattern(4);
