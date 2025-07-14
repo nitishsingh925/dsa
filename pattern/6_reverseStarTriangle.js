51bcf31bfe68f415c3a886e7fb08f714ba23db0c
Nitish Singh <nitishsingh925@gmail.com>
2025-07-14 17:05:56 +0530

    Reverse Star Triangle Pattern Right Angle

---
+++ pattern/6_reverseStarTriangle.js
@@ -0,0 +1,23 @@
+/*
+ * Reverse Star Triangle Pattern Right Angle
+ * This code generates a right angle triangle matrix of numbers based on the input size.
+ * It also counts the total number of stars printed.
+ */
+
+console.log("Reverse Star Triangle Pattern Right Angle");
+
+function starPattern(n) {
+  console.log("Number Pattern for Given Number  =", n);
+  let totalStar = 0;
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - i; j++) {
+      row = row + "*";
+      totalStar++;
+    }
+    console.log(row);
+  }
+  console.log("Total Star ==>", totalStar);
+}
+
+starPattern(4);
